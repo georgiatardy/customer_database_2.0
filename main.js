ddcbfbec173f969324a69ebb349035067fa6833c
Georgia Tardy <georgiatardyart@gmail.com>
2017-08-07 14:52:50 -0400

    made corrections to main.js file, made use of template literals to append customer info to page, instead of using appendChild

---
+++ main.js
@@ -0,0 +1,90 @@
+console.log("Hey there!")
+
+for (var i = 0; i < customers.results.length; i++) {
+  let mainPage = document.querySelector('body');
+
+  let grid = document.createElement('div');
+
+  let employeeDirectory = `
+  <ul>
+  <li><img src="${customers.results[i].picture.large}"></li>
+  <li>${customers.results[i].name.first} ${customers.results[i].name.last}</li>
+  <li>${customers.results[i].email}</li>
+  <li>${customers.results[i].location.street}</li>
+  <li>${customers.results[i].location.city}</li>
+  <li>${customers.results[i].location.state}</li>
+  <li>${customers.results[i].location.postcode}</li>
+  <li>${customers.results[i].phone}</li>
+</ul>
+  `;
+  grid.innerHTML += employeeDirectory;
+  mainPage.appendChild(grid);
+}
+
+
+
+
+
+
+
+
+
+// (function() {
+//   'use strict';
+//
+//   const allCustomers = document.querySelector('.customers');
+//
+//   fetch('https://randomuser.me/api/?format=json&results=12')
+//     .then(function(response) {
+//       return response.json()
+//     })
+//     .then(function(data) {
+//       customerData(data.results);
+//     });
+//
+//   function customerData(arr) {
+//     for (let i in arr) {
+//       const person = arr[i];
+//       let personName = person.name.first + " " + person.name.last;
+//       let personEmail = person.email;
+//       let personAddr1 = person.location.street;
+//       let personAddr2 = person.location.city + ", " + person.location.state + ", " + person.location.postcode;
+//       let personPhone = person.phone;
+//       let personSSN = person.value;
+//       let box = document.createElement('div');
+//       let img = document.createElement('img');
+//       img.setAttribute('src', person.picture.large);
+//       box.appendChild(img);
+//
+//       let name = document.createElement('p');
+//       name.textContent = personName.toUpperCase();
+//       name.classList.add('name');
+//       box.appendChild(name);
+//
+//       let email = document.createElement('p');
+//       email.textContent = personEmail.toUpperCase();
+//       email.classList.add('email');
+//       box.appendChild(email);
+//
+//       let address = document.createElement('p');
+//       address.innerHTML = personAddr1 + "<br>" + personAddr2;
+//       address.classList.add('address');
+//       box.appendChild(address);
+//
+//       let phone = document.createElement('p');
+//       phone.textContent = personPhone;
+//       phone.classList.add('phone');
+//       box.appendChild(phone);
+//       allCustomers.appendChild(box);
+//
+//       let ssn = document.createElement('ssn');
+//       ssn.textContent = personSSN;
+//       ssn.classList.add('ssn');
+//       box.appendChild(ssn);
+//       allCustomers.appendChild(box);
+//
+//     }
+//   }
+// })
+//
+// ();
